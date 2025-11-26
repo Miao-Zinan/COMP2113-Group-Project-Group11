7fc877482a5a865e661fddcbdf04ca0dee4182ae
SUN Gao, Simon <66070834+SimonSunCHN@users.noreply.github.com>
2025-11-26 16:36:18 +0800

    Add files via upload

---
+++ chest_generate.h
@@ -0,0 +1,20 @@
+#ifndef CHEST_H
+#define CHEST_H
+
+#include <vector>
+#include "pos.h"
+
+struct pos;
+
+class ChestGenerator {
+public:
+    static std::vector<pos> generateChests(
+        std::vector<std::vector<char>>& maze,
+        int startX, int startY,
+        int exitX, int exitY,
+        int difficulty,
+        char chestChar = '$'
+    );
+};
+
+#endif
